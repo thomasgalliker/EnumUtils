6b1116ba9d1a3a523761980bb4d63d0a9f6feb17
thomasgalliker <thomas@bluewin.ch>
2016-08-03 16:32:15 +0200

    Initial commit

---
+++ EnumUtils/EnumHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumUtils
+{
+    public static class EnumHelper
+    {
+        private static readonly Random Rng = new Random();
+
+        /// <summary>
+        /// Checks if the given type <typeparamref name="T"/> is an enum type.
+        /// </summary>
+        /// <typeparam name="T">Generic param type.</typeparam>
+        /// <returns>True if given type is an enum, otherwise, false.</returns>
+        public static bool IsEnum<T>()
+        {
+            return GetUnderlyingType<T>().IsEnum;
+        }
+
+        public static IEnumerable<TEnum> GetValues<TEnum>()
+        {
+            var enumType = GetUnderlyingType(typeof(TEnum));
+            ThrowIfNotEnum(enumType);
+
+            return Enum.GetValues(enumType).Cast<TEnum>().ToList();
+        }
+
+        public static TEnum Parse<TEnum>(string value)
+        {
+            var enumType = GetUnderlyingType(typeof(TEnum));
+            ThrowIfNotEnum(enumType);
+
+            return (TEnum) Enum.Parse(enumType, value, true);
+        }
+
+        public static TEnum TryParse<TEnum>(string value, bool ignoreCase = true) where TEnum : struct
+        {
+            var enumType = GetUnderlyingType(typeof(TEnum));
+            ThrowIfNotEnum(enumType);
+
+            TEnum returnValue;
+            Enum.TryParse(value, ignoreCase, out returnValue);
+
+            return returnValue;
+        }
+
+        public static TEnum GetRandom<TEnum>()
+        {
+            var values = Enum.GetValues(typeof(TEnum));
+            var item = Rng.Next(0, values.Length);
+            return (TEnum) values.GetValue(item);
+        }
+
+        public static Type GetUnderlyingType(Type nullableType)
+        {
+            var underlyingType = Nullable.GetUnderlyingType(nullableType);
+            if (underlyingType != null)
+            {
+                return underlyingType;
+            }
+
+            return nullableType;
+        }
+
+        public static Type GetUnderlyingType<T>()
+        {
+            return GetUnderlyingType(typeof(T));
+        }
+
+        private static void ThrowIfNotEnum(Type type)
+        {
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException($"Type {type.Name} must be an enum.");
+            }
+        }
+    }
+}
---
+++ Tests/EnumUtils.Tests/EnumHelperTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnumUtils.Tests.Testdata;
+using FluentAssertions;
+using Xunit;
+
+namespace EnumUtils.Tests
+{
+    public class EnumHelperTests
+    {
+        [Fact]
+        public void ShouldReturnTrueIfTypeIsAnEnum()
+        {
+            // Act
+            bool isEnum = EnumHelper.IsEnum<Weekday>();
+
+            // Assert
+            isEnum.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldReturnTrueIfTypeIsANullableEnum()
+        {
+            // Act
+            bool isEnum = EnumHelper.IsEnum<Weekday?>();
+
+            // Assert
+            isEnum.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfTypeIsNotAnEnum()
+        {
+            // Act
+            bool isEnum = EnumHelper.IsEnum<string>();
+
+            // Assert
+            isEnum.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldGetValuesFromEnum()
+        {
+            // Act
+            IEnumerable<Weekday> weekdays = EnumHelper.GetValues<Weekday>();
+
+            // Assert
+            weekdays.Should().NotBeNull();
+            weekdays.Should().HaveCount(7);
+        }
+
+        [Fact]
+        public void ShouldGetValuesFromNullableEnum()
+        {
+            // Act
+            IEnumerable<Weekday?> weekdays = EnumHelper.GetValues<Weekday?>();
+
+            // Assert
+            weekdays.Should().NotBeNull();
+            weekdays.Should().HaveCount(7);
+        }
+
+        [Fact]
+        public void ShouldGetRandom()
+        {
+            // Act
+            var randomWeekdays = new List<Weekday>
+            {
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+                EnumHelper.GetRandom<Weekday>(),
+            };
+
+
+            // Assert
+            var weekdayGroups = randomWeekdays.GroupBy(s => s).Select(g => new
+            {
+                Weekday = g.Key,
+                Count = g.Count()
+            });
+
+            foreach (var group in weekdayGroups)
+            {
+                group.Count.Should().BeLessThan(randomWeekdays.Count);
+            }
+        }
+
+        [Fact]
+        public void ShouldParseWithSuccess()
+        {
+            // Arrange
+            string enumString = "Thu";
+
+            // Act
+            var parsed = EnumHelper.Parse<Weekday>(enumString);
+
+            // Assert
+            parsed.Should().NotBeNull();
+            parsed.Should().Be(Weekday.Thu);
+        }
+
+        [Fact]
+        public void ShouldParseWithFailure()
+        {
+            // Arrange
+            string enumString = "non-existent-weekday";
+
+            // Act
+            Action action = () => EnumHelper.Parse<Weekday>(enumString);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>().Which.Message.Contains("Requested value \'"+ enumString + "\' was not found.");
+        }
+
+        [Fact]
+        public void ShouldTryParseWithSuccess()
+        {
+            // Arrange
+            string enumString = "Fri";
+
+            // Act
+            var parsed = EnumHelper.TryParse<Weekday>(enumString);
+
+            // Assert
+            parsed.Should().NotBeNull();
+            parsed.Should().Be(Weekday.Fri);
+        }
+
+        [Fact]
+        public void ShouldTryParseWithFailure()
+        {
+            // Arrange
+            string enumString = "non-existent-weekday";
+
+            // Act
+            var parsed = EnumHelper.TryParse<Weekday>(enumString);
+
+            // Assert
+            parsed.Should().NotBeNull();
+            parsed.Should().Be(default(Weekday));
+        }
+    }
+}
