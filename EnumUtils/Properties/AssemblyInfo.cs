d3494b7423187357140b499780ccf4ace617f5d5
thomasgalliker <thomas@bluewin.ch>
2016-12-25 18:27:19 +0100

    Release next stable version 1.0.3

--- EnumUtils/Properties/AssemblyInfo.cs
+++ EnumUtils/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3-pre3")]
+[assembly: AssemblyFileVersion("1.0.3")]
